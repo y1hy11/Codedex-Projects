48603338797d3c9b45ff70b4f5fad3208c031ac8
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-06-17 20:02:54 +0100

    horoscope with js
    first js project i have done :)

---
+++ JavaScript/Horoscope.js
@@ -0,0 +1,30 @@
+let Month = 12
+let fortunes= ["You will have a great day!","Good things are coming your way!","Be carefuli, 'something unexpected m'ght happen!","You will meet someone new today!","You will have a surprise party tonight!","You will get a promotion at work!","You will find a $20 bill on the street!","You will have a wonderful weekend!"]
+
+if (Month === 1) {
+  console.log('Capricorn', fortunes [0])
+} else if (Month === 2) {
+console.log('Aquarius', fortunes [1])
+} else if (Month === 3) {
+ console.log('Pisces', fortunes [2])
+ } else if (Month === 4) {
+ console.log('Aries', fortunes [3])
+ } else if (Month === 5) {
+ console.log('Taurus', fortunes [4])
+ } else if (Month === 6) {
+ console.log('Gemini', fortunes [5])
+ } else if (Month === 7) {
+ console.log('Cancer', fortunes [6])
+ } else if (Month === 8) {
+ console.log('Leo',fortunes [7])
+ } else if (Month === 9) {
+ console.log('Virgo', fortunes [3])
+ } else if (Month === 10) {
+ console.log('Libra', fortunes [2])
+ } else if (Month === 11) {
+ console.log('Scorpio', fortunes [5])
+ } else if (Month === 12) {
+ console.log('Sagittarius', fortunes [6])
+ } else {
+ console.log("Invalid Month")
+}

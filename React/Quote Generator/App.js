10a44fce34d83ac6eaf33825e9fd1ec85d58d7b0
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-19 22:29:53 +0100

    chore: Add QuoteGenerator component to App.js

---
+++ React/Quote Generator/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuoteGenerator from './QuoteGenerator';
+
+function App() {
+  return (
+    <div>
+      <QuoteGenerator />
+    </div>
+  );
+}
+
+export default App;

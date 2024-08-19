ad81906295062f9c66b6f5079608c75b04387fb1
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-08-19 22:26:03 +0100

    "Added QuoteGenerator component with quote array, state, and generateQuote function"

---
+++ React/Quote Generator/Quote Generator.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const QuoteGenerator = () => {
+  const quotes = [
+    "You can't use up creativity. The more you use, the more you have. - Maya Angelou",
+    "The best way to predict the future is to create it. - Peter Drucker",
+    "Let us pick up our books and our pens, they are the most powerful weapons. - Malala Yousafzai",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "It always seems impossible until it’s done. - Nelson Mandela",
+    "I am not afraid... I was born to do this. - Joan of Arc",
+    "Believe you can and you’re halfway there. - Theodore Roosevelt"
+  ];
+
+  const [currentQuote, setCurrentQuote] = useState('');
+
+  const generateQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setCurrentQuote(quotes[randomIndex]);
+  };
+
+  return (
+    <div>
+      <h1>Random Quote Generator</h1>
+      <button onClick={generateQuote}>Generate Quote</button>
+      <p>{currentQuote}</p>
+    </div>
+  );
+};
+
+export default QuoteGenerator;

a57cb4acf7dc692e7d0b605baab710a4adc9196f
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-09-20 00:34:39 +0100

    Added Header component with navigation links

---
+++ React/The-Personality-Quiz/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Personality Quiz</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/quiz">Quiz</Link>
+      </nav>
+    </header>
+  );
+}

4b5763a7c0018834b869f340fa259a3a4cddd37b
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-09-20 00:33:52 +0100

    Added Footer component with a 'Made with ❤, by y1hy11' message

---
+++ React/The-Personality-Quiz/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      Made with <span style={{ color: 'red' }}>❤</span>,  by y1hy11
+    </footer>
+  );
+};
+
+export default Footer;

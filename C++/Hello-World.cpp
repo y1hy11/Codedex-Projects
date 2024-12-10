d973cf5c9bbe8af1d4ea16d83ea0fb0cdd88c892
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-12-10 16:58:54 +0000

    Fix output statement in Hello World program to include std::endl for proper line termination

--- C++/Hello-World.cpp
+++ C++/Hello-World.cpp
@@ -5,7 +5,6 @@ int main() {
   std::cout << "\n"; //it makes a new empty line its called "Escape Sequence"
   std::cout << "My Name is Yahya"; //it prints my name
   std::cout << "\t"; //it makes a tab space 
-  std::cout << "And This is my First C++ Program Code That I Wrote :)"; //it prints a message 
-  std::endl
+  std::cout << "And This is my First C++ Program Code That I Wrote :)" << std::endl;
   return 0;
   }

4db80e00a95c86ecb3faf260c966e8486a127749
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-12-10 17:56:23 +0000

    Add Bio.cpp to introduce a personal introduction and showcase frontend development passion

---
+++ C++/Bio.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+    // Write code here
+
+    std::string name = "Yahya 👋";
+    std::string country = "Morocco 🇲🇦";
+    std::string technologie = "Frontend";
+
+    std::cout << "Hello,I'm " << name << " | Frontend Developer from " << country << ". I'm passionate about Transforming ideas into elegant digital experiences through code. With a passion for " << technologie << " technologies and an unwavering commitment to learning, I craft responsive, user-centric web solutions. My journey is driven by curiosity, innovation, and the continuous pursuit of technical excellence. I'm dedicated to building intuitive interfaces that not only look great but also provide seamless user interactions." << std::endl;
+    return 0;
+}

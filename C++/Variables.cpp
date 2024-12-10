34291c7c1176ca269e2f2fd3ad9cf6f5877a1916
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-12-10 17:06:44 +0000

    Add Variables.cpp to demonstrate variable types in C++

---
+++ C++/Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    char point = ','; // it's a character
+    int age = 18; // it's an integer
+    float pi = 3.14; // it's a float
+    double pi2 = 3.14159265359; // it's a double
+    bool honor = true; // it's a boolean
+    std::string name = "Yahya" << std::endl; // it's a string 
+    return 0;
+}

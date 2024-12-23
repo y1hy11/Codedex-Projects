8e54c9bfebc1bd163f1f06b235f62c4638d12338
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-12-23 18:00:13 +0000

    Add C++ program for random heads or tails generation

---
+++ C++/RasOlaTaj.cpp
@@ -0,0 +1,17 @@
+// RasOlaTaj Means Heads OR Tails in english
+
+#include <iostream> // For std::cout
+#include <cstdlib> // For std::rand() and std::srand()
+
+int main() {
+  srand(time(NULL));
+
+  int num = std::rand() % 2;  // Generates a random number that's either 0 or 1
+
+  if (num > 0.5) { 
+    std::cout << "Ras\n"; //Head
+  }
+  else { 
+    std::cout << "Taj\n"; //Tail
+  }
+}

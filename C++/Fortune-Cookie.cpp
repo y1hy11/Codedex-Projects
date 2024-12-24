6543f5b7a593ca2dd66783c15ccde5a7b67e347b
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2024-12-24 10:03:59 +0000

    Enhance Fortune Cookie program with comments for clarity

--- C++/Fortune-Cookie.cpp
+++ C++/Fortune-Cookie.cpp
@@ -1,13 +1,15 @@
+// this code is a simple fortune cookie program that generates a random fortune cookie message each time it's run.
+
 #include <iostream>
 #include <cstdlib>
 
 int main(){
-  srand(time(NULL));
+  srand(time(NULL)); // Seeds the random number generator
 
-  int Number = std::rand() % 7;  
+  int Number = std::rand() % 7;  // Generates a random number that's either 0, 1, 2, 3, 4, 5, 6
 
 if (Number == 1){
-  std::cout << "Don't pursue happiness – create it.\n";
+  std::cout << "Don't pursue happiness – create it.\n"; 
 } else if (Number == 2) {
   std::cout << "All things are difficult before they are easy.\n";
 } else if (Number == 3) {
@@ -17,7 +19,7 @@ if (Number == 1){
 } else if (Number == 5) {
   std::cout << "The fortune you search for is in another cookie.\n";
 } else if (Number == 6) {
-  std::cout << "Help! I'm being held prisoner in a Chinese bakery!\n";
+  std::cout << "Help! I'm being held prisoner in a Chinese bakery!\n"; // This is a joke from the fortune cookie "Sounds like a joke but it's not ;)"
 } else {
   std::cout << "🥠 The fortune you search for is in another cookie.\n";
   return 0;

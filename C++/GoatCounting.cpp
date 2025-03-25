f491ad3659fcf38ca3b95ff764f79db60a229c18
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-03-25 00:40:57 +0000

    Add C++ program to count goats in a specified range

---
+++ C++/GoatCounting.cpp
@@ -0,0 +1,10 @@
+// this code shows how to count the number of goats in a given range
+
+#include <iostream>
+
+int main() {
+
+  for(int i = 1; i <= 111; i++) {
+    std::cout << i << " Goat \n";
+  }
+}
